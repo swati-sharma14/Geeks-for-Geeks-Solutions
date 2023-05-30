fd3200e94bb9e2eed0eda41e0d5e559f366be7ac
Swati Sharma <103050981+swati07sharma@users.noreply.github.com>
2023-05-30 20:07:42 +0530

    https://practice.geeksforgeeks.org/problems/smallest-subarray-with-sum-greater-than-x5651/0

---
+++ Easy/Smallest subarray with sum greater than x.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+
+    int smallestSubWithSum(int arr[], int n, int x)
+    {
+        // Your code goes here   
+        int i = 0, j = 0, s = 0, ans = n;
+        
+        while(j < n){
+            s += arr[j];
+            while(s > x && i <= j){
+                ans = min(ans, j-i+1);
+                s -= arr[i++];
+            }
+            j++;
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,x;
+		cin>>n>>x;
+		int a[n];
+		for(int i=0;i<n;i++)
+		cin>>a[i];
+		Solution obj;
+		cout<<obj.smallestSubWithSum(a,n,x)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
