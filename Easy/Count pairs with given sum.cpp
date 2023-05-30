9caa763d4a9ce71919c589f75acb8bb8f2337828
Swati Sharma <103050981+swati07sharma@users.noreply.github.com>
2023-05-30 20:04:00 +0530

    https://practice.geeksforgeeks.org/problems/count-pairs-with-given-sum5022/0

---
+++ Easy/Count pairs with given sum.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int getPairsCount(int arr[], int n, int k) {
+        map<int,int> numbers;
+        for(int i=0; i<n; i++){
+            numbers[arr[i]] += 1;
+        }
+        int count = 0;
+        for(int i=0; i<n; i++){
+            int a = k - arr[i];
+            numbers[arr[i]] -= 1;
+            count += numbers[a];
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getPairsCount(arr, n, k);
+        cout << ans << "\n";
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
