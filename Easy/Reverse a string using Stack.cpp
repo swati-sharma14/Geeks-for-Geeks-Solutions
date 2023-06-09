47bb8eadb442672e16d839b7c206e0136f0c8fc4
Swati Sharma <103050981+swati07sharma@users.noreply.github.com>
2023-06-09 12:24:43 +0530

    https://practice.geeksforgeeks.org/problems/reverse-a-string-using-stack/0

---
+++ Easy/Reverse a string using Stack.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+char* reverse(char *str, int len);
+int main()
+{
+    long long int t;
+    cin>>t;
+    while(t--)
+    {
+        char str[10000];
+        cin>>str;
+        long long int len=strlen(str);
+        char *ch=reverse(str,len);
+        for(int i=0;i<len;i++)
+        {
+            cout<<ch[i];
+        }
+        cout<<endl;
+    }
+        return 0;
+}
+
+
+// } Driver Code Ends
+
+
+//return the address of the string
+char* reverse(char *S, int len)
+{
+    stack<char> s;
+    for(int i=0; i<len; i++){
+        s.push(S[i]);
+    }
+    char* ans = new char[len];
+    int i = 0;
+    while(!s.empty()){
+        ans[i++] = s.top();
+        s.pop();
+    }
+    ans[i] = '\0';
+    return ans;
+}
